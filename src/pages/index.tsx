517a1937b07908c57863e140479b075b3f20c5a7
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-02-24 21:31:51 -0300

    components finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
-import Head from 'next/head'
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import styles from "../styles/pages/Home.module.css";
+
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Head>
+        <title>Início | move.it</title>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
       </Head>
-      <ExperienceBar/>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+
+        <div></div>
+      </section>
     </div>
   )
 }
