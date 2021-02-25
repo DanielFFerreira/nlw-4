db22b1b02cabcd5c4bc01d837030ee1bd5b24543
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-02-25 18:44:22 -0300

    include first business rules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,9 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Início | move.it</title>
+        <title>
+          Início | move.it
+        </title>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
       </Head>
