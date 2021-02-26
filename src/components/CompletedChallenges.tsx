3ddb4dab85b020cdf22c8b5dbed14bf0d00b2876
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-02-26 16:07:19 -0300

    using context API

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,15 @@
+import { useContext } from 'react';
+import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext';
 import styles from '../styles/components/CompletedChallenges.module.css';
+import { ChallengeBox } from './ChallengeBox';
 
 export function CompletedChallenges() {
+  const { challengesCompleted } = useContext(ChallengesContext);
+
   return (
     <div className={styles.CompletedChallengesContainer}>
       <span>Desafios completos</span>
-      <span>5</span>
+      <span>{challengesCompleted}</span>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { Profile } from "../components/Profile";
 import styles from "../styles/pages/Home.module.css";
 
 import Head from 'next/head';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 export default function Home() {
   return (
@@ -24,8 +25,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   )
